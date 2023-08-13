cb7de7bc9ed77788b5897ee016739627f974f580
Graham Knop <haarg@haarg.org>
2023-08-13 22:52:10 +0200

    replace "standard" eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: [
-    'standard-with-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
     'plugin:prettier/recommended',
   ],
